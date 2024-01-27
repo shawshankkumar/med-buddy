947f7ad0cbc8f6b88df9563b91fc672cf665314d
shawshankkumar <shashankkumarthakur@gmail.com>
2024-01-27 20:35:07 +0530

    Add CORS middleware and health check route

--- main.py
+++ main.py
@@ -1,8 +1,30 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+import time
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+global_time = time.time()
+
+
+@app.get("/health", tags=["health"])
+def health_check():
+    return {
+        "status": "ok",
+        "message": "med-buddy is running",
+        "application_name": "med buddy",
+        "uptime_in_seconds": time.time() - global_time,
+    }
 
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Index Route (/)"}
