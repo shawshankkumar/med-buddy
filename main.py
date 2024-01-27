ccb691d6d97a1473639fb7bc7df6f09dd4318083
shawshankkumar <shashankkumarthakur@gmail.com>
2024-01-28 00:37:00 +0530

    Add file upload functionality and integrate with AWS S3

--- main.py
+++ main.py
@@ -1,9 +1,22 @@
-from fastapi import FastAPI
+import io
+from fastapi import FastAPI, File, UploadFile, Request, status
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
 import time
 import ulid
+import boto3
+from util import check_validation 
 
 app = FastAPI()
+
+bucket_name = 'shashank'
+s3 = boto3.client(
+    "s3",
+    endpoint_url="https://61837c45dfc997b1a932f4b2116d93ae.r2.cloudflarestorage.com",
+    aws_access_key_id="8d1d7da927b6a897dd462fe69b46954e",
+    aws_secret_access_key="b5cfdb89b62166644f07df75f8b5e14c8a22f40ae5de2319a4cc9ee133b9551a",
+    region_name="auto",
+)
 
 app.add_middleware(
     CORSMiddleware,
@@ -28,9 +41,15 @@ def health_check():
 
 @app.get("/")
 async def root():
-    return {"message": "Index Route (/)"}
+    return { "message" : "index route of med buddy (/)" }
 
 
 @app.post("/upload")
-async def upload():
-    return {"message": "File uploaded", "code": ulid.new()}
+async def upload(request: Request, file: UploadFile = File(...)):
+    user_id = request.headers.get('Authorization')
+    if(not check_validation(user_id)):
+        return JSONResponse(status_code = 401, content = {"message": "Auth headers are required"})
+    content = await file.read()
+    file_key = 'file_' + str(ulid.new())
+    s3.upload_fileobj(io.BytesIO(content), bucket_name, user_id + '/' + file_key)
+    return {"message": "File uploaded", "code": file_key}
--- util.py
+++ util.py
@@ -1,4 +1,4 @@
-tokens = ["89f49d10-346b-4b31-9f79-ff3bd840cc6f"]
+tokens = ["token_89f49d10-346b-4b31-9f79-ff3bd840cc6f"]
 
 
 def check_validation(token):
