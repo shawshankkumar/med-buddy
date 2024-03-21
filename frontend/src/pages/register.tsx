b3e32ed3b4ad94ae9f4492d93f62c30ec6db15d2
shawshankkumar <shashankkumarthakur@gmail.com>
2024-03-21 23:00:24 +0530

    Add Toaster component for displaying toast notifications

--- frontend/src/pages/register.tsx
+++ frontend/src/pages/register.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
@@ -8,52 +8,129 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
+} from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
 import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select"
- 
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { useToast } from "@/components/ui/use-toast";
+import axios from "axios";
+
+const FormSchema = z.object({
+  name: z.string().min(2, {
+    message: "name must be at least 2 characters.",
+  }),
+  email: z.string().email(),
+  password: z.string().min(2, {
+    message: "password must be at least 2 characters.",
+  }),
+});
+
 export default function CardWithForm() {
+  const { toast } = useToast();
+
+  async function onSubmit(data: z.infer<typeof FormSchema>) {
+    console.log("inputData: ", data);
+    await axios.post(process.env.NEXT_PUBLIC_API_ENDPOINT + "/register", data);
+    toast({
+      title: "Uh oh! Something went wrong.",
+      description: "There was a problem with your request.",
+    });
+  }
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      name: "",
+      email: "",
+      password: "",
+    },
+  });
+
   return (
-    <Card className="w-1/2">
-      <CardHeader>
-        <CardTitle>Create project</CardTitle>
-        <CardDescription>Deploy your new project in one-click.</CardDescription>
-      </CardHeader>
-      <CardContent>
-        <form>
-          <div className="grid w-full items-center gap-4">
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="name">Name</Label>
-              <Input id="name" placeholder="Name of your project" />
-            </div>
-            <div className="flex flex-col space-y-1.5">
-              <Label htmlFor="framework">Framework</Label>
-              <Select>
-                <SelectTrigger id="framework">
-                  <SelectValue placeholder="Select" />
-                </SelectTrigger>
-                <SelectContent position="popper">
-                  <SelectItem value="next">Next.js</SelectItem>
-                  <SelectItem value="sveltekit">SvelteKit</SelectItem>
-                  <SelectItem value="astro">Astro</SelectItem>
-                  <SelectItem value="nuxt">Nuxt.js</SelectItem>
-                </SelectContent>
-              </Select>
-            </div>
-          </div>
-        </form>
-      </CardContent>
-      <CardFooter className="flex justify-between">
-        <Button variant="outline">Cancel</Button>
-        <Button>Deploy</Button>
-      </CardFooter>
-    </Card>
-  )
+    <div className="w-screen mt-52">
+      <Card className="w-1/2 m-auto shadow-2xl">
+        <CardHeader>
+          <CardTitle className="text-[#023382] font-bold text-2xl">
+            Register now
+          </CardTitle>
+          <CardDescription className="">
+            Once you register, we provide a complete suite of tools to manage
+            your medical reports
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem className="mb-6">
+                    <FormLabel className="text-[#023382]">Name</FormLabel>
+                    <FormControl>
+                      <Input placeholder="James Bond" {...field} />
+                    </FormControl>
+                    <FormDescription>
+                      This is your public display name.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem className="mb-6">
+                    <FormLabel className="text-[#023382]">Email</FormLabel>
+                    <FormControl>
+                      <Input placeholder="james.bond@mi6.com" {...field} />
+                    </FormControl>
+                    <FormDescription>This is your login email</FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel className="text-[#023382]">Password</FormLabel>
+                    <FormControl>
+                      <Input
+                        placeholder="If I tell you, I'll have to kill you"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormDescription>
+                      This is your password, keep it safe.
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <div className="flex justify-between mt-4">
+                <Button variant="outline">Cancel</Button>
+                <Button className="bg-[#023382]">Submit</Button>
+              </div>
+            </form>
+          </Form>
+        </CardContent>
+        <CardFooter></CardFooter>
+      </Card>
+    </div>
+  );
 }
